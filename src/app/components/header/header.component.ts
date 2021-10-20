85a919a84b27e2179bca7a57ebde0bb98b16ffe0
renan-alves <renan.a.a.costa@gmail.com>
2021-10-20 20:22:48 -0300

    Corrigindo alguns bugs

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -15,9 +15,11 @@ export class HeaderComponent implements OnInit {
 
   constructor(private afAuth: AngularFireAuth) {
     this.afAuth.authState.subscribe(user => {
-      if (user && !user.isAnonymous)
+      if (user && !user.isAnonymous && user.emailVerified)
         this.isLoggedIn = true;
       else this.isLoggedIn = false;
+
+      console.log(this.isLoggedIn);
 
       this.showMenu = true;
     })
